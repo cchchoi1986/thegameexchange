53f9ddf6513ddff83dc663b976e330934f45fd3c
cchchoi1986 <cchchoi1986@gmail.com>
2015-01-07 17:02:07 +0800

    next get games

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,31 @@
 
 
 # creates default users
-user0 = User.create(:email => "cchchoi1986@gmail.com", :password => 'thisisatest', :password_confirmation => "thisisatest", :username => 'bigbrother801')
+user0 = User.create(:email => "cchchoi1986@gmail.com", :password => 'thisisatest', :password_confirmation => "thisisatest", :username => 'bigbrother801', :location => 'Hong Kong')
 
-user1 = User.create(:email => "user1@none.com", :password => '12345678', :password_confirmation => "12345678", :username => 'littlebro01')
+user1 = User.create(:email => "user1@none.com", :password => '12345678', :password_confirmation => "12345678", :username => 'littlebro01', :location => 'Singapore')
 
-user2 = User.create(:email => "user2@none.com", :password => '12345678', :password_confirmation => "12345678", :username => 'littlebro02')
+user2 = User.create(:email => "user2@none.com", :password => '12345678', :password_confirmation => "12345678", :username => 'littlebro02', :location => 'Hong Kong')
 
-user3 = User.create(:email => "user3@none.com", :password => '12345678', :password_confirmation => "12345678", :username => 'littlebro03')
+user3 = User.create(:email => "user3@none.com", :password => '12345678', :password_confirmation => "12345678", :username => 'littlebro03', :location => 'Arctic Circle')
 
 # creates default posts linked to a user_id
-user_game1 = UserGame.create(game_id: 14, wantgive: 'w', condition: 3, user_id: user1[:id])
+user_game1 = UserGame.create(game_id: 1122, wantgive: 'w', condition: 3, user_id: user1[:id])
 
-user_game2 = UserGame.create(game_id: 12, wantgive: 'g', condition: 5, user_id: user3[:id])
+user_game2 = UserGame.create(game_id: 1122, wantgive: 'g', condition: 5, user_id: user3[:id])
 
-user_game3 = UserGame.create(game_id: 12, wantgive: 'w', condition: 3, user_id: user2[:id])
+user_game3 = UserGame.create(game_id: 5132, wantgive: 'w', condition: 3, user_id: user2[:id])
 
-user_game4 = UserGame.create(game_id: 13, wantgive: 'w', condition: 5, user_id: user2[:id])
+user_game4 = UserGame.create(game_id: 4132, wantgive: 'w', condition: 5, user_id: user2[:id])
 
-user_game5 = UserGame.create(game_id: 15, wantgive: 'g', condition: 4, user_id: user1[:id])
+user_game5 = UserGame.create(game_id: 2151, wantgive: 'g', condition: 4, user_id: user1[:id])
 
-user_game6 = UserGame.create(game_id: 15, wantgive: 'g', condition: 5, user_id: user3[:id])
+user_game6 = UserGame.create(game_id: 1542, wantgive: 'g', condition: 5, user_id: user3[:id])
 
-user_game7 = UserGame.create(game_id: 14, wantgive: 'g', condition: 4, user_id: user3[:id])
+user_game7 = UserGame.create(game_id: 1124, wantgive: 'g', condition: 4, user_id: user3[:id])
+
+user_game8 = UserGame.create(game_id: 251, wantgive: 'g', condition: 4, user_id: user0[:id])
+
+user_game9 = UserGame.create(game_id: 2151, wantgive: 'g', condition: 5, user_id: user0[:id])
+
+user_game10 = UserGame.create(game_id: 1122, wantgive: 'g', condition: 4, user_id: user0[:id])
